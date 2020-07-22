0482c232da4740798c847de655a43838b56c6366
MaciborekGrzesiek <maciborek.grzegorz@gmail.com>
2020-07-22 18:36:33 +0200

    initial commit, start game

--- main.py
+++ main.py
@@ -1,22 +1,42 @@
 import pygame
 import os
 
+#  start game
 pygame.init()
 WIDTH = 800
 HEIGHT = 500
-pygame.display.set_mode((WIDTH, HEIGHT))
+win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("HaNgMaN gAmE!")
 
+#  img
+image = pygame.image.load('./img/hangman6.png')
+images = [image]
+# for i in range(1):
+#     image = pygame.image.load('./img/hangman' + str(i) + '.png')
+#     images.append(image)
+hangman_image = 0
+
+
+#  colors
+WHITE = (255, 255, 255)
+
+#  setup game loop
 FPS = 60
 clock = pygame.time.Clock()
-
 run = True
 
 while run:
     clock.tick(FPS)
 
+    win.fill(WHITE)
+    win.blit(images[hangman_image], (100, 100))
+    pygame.display.update()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pos = pygame.mouse.get_pos()
+            print(pos)
 
 pygame.quit()
